e3b8188b4428c6cb393d85ee5dfa4c785b6ee5ca
Shawn Tapps <stapps.prsvr@gmail.com>
2024-06-05 21:37:45 -0500

    we are variable complete

--- JS/assignments/Week1/we-are-vari-able/main.js
+++ JS/assignments/Week1/we-are-vari-able/main.js
@@ -1,8 +1,18 @@
 /******************
  * YOUR CODE BELOW *
  ******************/
-
-
+let myFirstName = "Shawn"
+let myLastName = "Tapps"
+let myFavoriteCeleb = "Kate"
+let mySentence = ("I," + " " + myFirstName + " " + myLastName + " " + "do enjoy a movie with" + " " + myFavoriteCeleb + " " +"in it!")
+let myLuckyNum = 27
+let myAge = 44
+let myHeight = 68
+let myFavoriteYear = 1979
+let mySum = (5 + 2317 + 36)
+let myProduct = ((232 * 43)/2)+4
+let myHeightAndAge = myAge + myHeight
+console.log (myFirstName, myLastName, myFavoriteCeleb, mySentence, myLuckyNum, myAge, myHeight, myFavoriteYear, mySum, myProduct, myHeightAndAge)
 /*********************************************************************************************************************
 
  * CODE BELOW HERE IS FOR INTERNAL USE ONLY! IT ENSURES THAT THE TESTING *
