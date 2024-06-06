f5f06ac5f34aefb08f39d48422e368175305a5c7
Shawn Tapps <stapps.prsvr@gmail.com>
2024-06-05 21:40:49 -0500

    Update to we are variable

---
+++ Algos/filterOddsAndEvens.js
@@ -0,0 +1,32 @@
+// Finish the function to filter out all odd numbers and all even numbers from an array of numbers.
+
+
+// Example
+// filterOddsAndEvens([1,2,3,4,5,6,7,8,9,10]) should return [2,4,6,8,10]
+
+// Hint
+
+
+// Pseudo Code
+    
+
+
+
+function filterOddsAndEvens(arr){
+    let even = []
+    let odd = []
+for (let i=0; i<arr.length; i++){
+    if (arr[i]%2 === 0){
+        even.push(arr[i])
+}
+else{
+    odd.push(arr[i])
+}
+
+
+}
+
+}
+console.log(filterOddsAndEvens([1,2,3,4,5,6,7,8,9,10])); // [2,4,6,8,10]
+
+
