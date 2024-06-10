ae17ec722a3841194d5cec78f1f0b5fe7363dced
Shawn Tapps <stapps.prsvr@gmail.com>
2024-06-10 09:18:01 -0500

    update week 5

--- JS/assignments/Week4/hip-hip-array/main.js
+++ JS/assignments/Week4/hip-hip-array/main.js
@@ -22,7 +22,10 @@ const students = [
   'Arnold',
   'Andy'
 ];
-
+function arrLoop(students) {
+  for (let i = 0; i < students.length; i++) { return students }
+}
+console.log(students)
 
 // Challenge 2
 // Loop through the following array BACKWARDS, console.logging out each value. 64 should be printed first and 100 last.
